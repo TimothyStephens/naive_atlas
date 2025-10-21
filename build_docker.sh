90c1e3c0103e2e50c73a482f16dc9f80d180c2e3
Timothy Stephens <ts942@sebs.rutgers.edu>
2025-10-21 09:18:14 -0400

    Added singulairty support

--- build_docker.sh
+++ build_docker.sh
@@ -15,6 +15,7 @@ VERSION=$(git describe --tags --dirty --always)
 echo -e "# Dockerfile to containerize naiveATLAS workflow
 FROM condaforge/mambaforge:latest
 
+
 ## (1/7) Set environment variables
 # So we dont have to interactivly configure tzdata
 RUN ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
@@ -29,6 +30,7 @@ RUN apt-get update && \\
       zlib1g zlib1g-dev build-essential gcc && \\
     rm -rf /var/lib/apt/lists/*
 
+
 ## (2/7) Install Singulairty
 RUN apt-get update && \\
     apt-get install -y --no-install-recommends \\
@@ -40,6 +42,7 @@ RUN apt-get update && \\
     && apt-get install -y apptainer \\
     && rm -rf /var/lib/apt/lists/*
 
+
 ## (3/7) Create and set workflow working directory
 WORKDIR /app
 COPY . .
@@ -47,6 +50,7 @@ RUN mamba env create --prefix /conda-envs/naive_atlas --file naive_atlasenv.yml
 SHELL [\"conda\", \"run\", \"-p\", \"/conda-envs/naive_atlas\", \"/bin/bash\", \"-c\"]
 RUN /conda-envs/naive_atlas/bin/pip3 install --prefix /conda-envs/naive_atlas --editable .
 SHELL [\"/bin/sh\", \"-c\"]
+
 
 ## (4/7) Install each workflow package" > "$DOCKERFILE"
 
@@ -61,12 +65,12 @@ MD5SUM=$(md5sum "$YAML" | awk '{print $1}')
 if ! grep -qFx "$MD5SUM" "$tmp_seen"; then
   # Not seen
 
-echo -e "
-# Conda environment:
+echo -e "# Conda environment:
 #   source: $YAML
 #   prefix: /conda-envs/$MD5SUM" >> "$DOCKERFILE"
 awk '{print "#   "$0}' "$YAML"   >> "$DOCKERFILE"
 echo -e "COPY $YAML /conda-envs/$MD5SUM.yaml" >> "$DOCKERFILE"
+echo "" >> "$DOCKERFILE"
 
 # Need to run mamba create as each RUN creates a new layer, which is a lot of diskspace overhead and will cause issues if done separatly.
 NL=$'\n'
@@ -80,34 +84,67 @@ fi
 
 else
 
-echo -e "
-# Conda environment:
+echo -e "# Conda environment:
 #   source: $YAML
 #   prefix: /conda-envs/$MD5SUM" >> "$DOCKERFILE"
 awk '{print "#   "$0}' "$YAML"   >> "$DOCKERFILE"
 echo -e "# NOTRUN: Seen md5sum previously" >> "$DOCKERFILE"
+echo "" >> "$DOCKERFILE"
 
 fi
 
 done
 RUN="${RUN}${NL}    mamba clean -afy"
+rm "$tmp_seen"
+echo -e "$RUN" >> "$DOCKERFILE"
 
+
+
+echo -e '
+
+## (5/7) Download Singulairty/Apptainer SIF files
+RUN mkdir -p /containers-sif' >> "$DOCKERFILE"
+
+RUN=""
+tmp_seen=$(mktemp)
+for URI in $(cat workflow/rules/*.smk \
+  | grep 'docker:' \
+  | awk '{print $1}' \
+  | sed -e 's/,//'); do
+
+HASH=$(echo -n "$URI" | sha256sum | awk '{print $1}')
+SIF="/containers-sif/${HASH}.sif"
+
+if ! grep -qFx "$HASH" "$tmp_seen"; then
+  # Not seen
+  NL=$'\n'
+  if [ -z "$RUN" ]; then
+    RUN="RUN apptainer pull --name $SIF $URI && \\"
+  else
+    RUN="${RUN}${NL}    apptainer pull --name $SIF $URI && \\"
+  fi
+  
+  echo "$HASH" >> "$tmp_seen"
+fi
+
+done
+
+RUN="${RUN}${NL}    apptainer cache clean --force"
 rm "$tmp_seen"
 echo -e "\n$RUN" >> "$DOCKERFILE"
 
+
 echo -e '
-## (5/7) Cleanup and verify the installation by checking the version
-RUN mamba clean --all -y
 
 ## (6/7) Verify the installation by checking the version
 RUN naive_atlas --help
+
 
 ## (7/7) Set the default command to show MaxBin2 help
 CMD ["naive_atlas", "--help"]
 ' >> "$DOCKERFILE"
 
 
-#exit 0
 ## Build using docker
 $DOCKER build -t $USER/naive_atlas:${VERSION} .
 $DOCKER run $USER/naive_atlas:${VERSION} naive_atlas --help
